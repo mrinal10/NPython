dabc615289f39c9279960a7d71ffbccb0e44528f
mailm <mrinal.mondal6@gmail.com>
2018-03-01 00:04:38 +0530

    ReverseString

--- com/mrinal/new/ReverseString.py
+++ com/mrinal/new/ReverseString.py
@@ -4,14 +4,20 @@ Created on 28-Feb-2018
 @author: mailm
 '''
 
-class MyClass(object):
-    '''
-    classdocs
-    '''
+class StringReverse:
+    stringValue=""
 
 
-    def __init__(self, params):
-        '''
-        Constructor
-        '''
-        
+    def __init__(self, stringVal):
+        self.stringValue = stringVal
+    
+    def reverseString(self):
+        str = ""
+        for i in self.stringValue:
+            str = i + str
+        return str
+
+newStringObj = StringReverse("lanirM")
+  
+reversedString = newStringObj.reverseString();
+print(reversedString)
